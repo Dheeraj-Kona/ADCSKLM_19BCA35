73ccddf5b93b476f1bd43c53b2ac437064428b58
Dheeraj Kona <66221964+Dheeraj-Kona@users.noreply.github.com>
2021-06-29 21:32:53 +0530

    Update Reverse Number(29-6-21).java

--- Reverse Number(29-6-21).java
+++ Reverse Number(29-6-21).java
@@ -1,19 +1,26 @@
-import java.io.*;
 import java.util.*;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int r ,rev = 0;
-        while(n!=0){
-            r = n%10;
-            rev=rev*10+r;
-            n=n/10;
-        }
+class Solution
+{
     
-        System.out.println(rev);
+    static int reversDigits(int num)
+    {
+        int r,rev = 0;
         
+        while(num != 0)
+        {
+            
+            r = num%10;
+            rev=rev*10+r;
+            num=num/10;
+        }
+        
+        return rev;
+    }
+     
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.print(reversDigits(num));
     }
 }
